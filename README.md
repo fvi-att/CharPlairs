579153a5cadd778d0f012ce0a2fdd684c703de22
shimizu <jshimizujp@gmail.com>
2018-12-06 17:09:37 +0900

    [feature] Add base64, ROT13, base58

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CharPlairs
 CharPlairs is a small tool for finding clue from some string 
+
+## install 
+```bash
+$ cd /usr/local/bin && git clone https://github.com/fvi-att/CharPlairs.git 
+```
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import base64
+import binascii
+import codecs
 
 
 if __name__ == "__main__":
@@ -7,6 +9,27 @@ if __name__ == "__main__":
         print("you should input something....")
         exit(1)
 
-    # Base64
-    print(f"Base64(Byte): {base64.b64decode(words)}")
-    print(f"Base64(UTF-8): {base64.b64decode(words).decode(encoding='utf-8')}")
+    matched = []
+    print("=================  Base64  =================")
+    try:
+        print(f"| Base64(Byte): {base64.b64decode(words)}")
+        print(f"| Base64(UTF-8): {base64.b64decode(words).decode(encoding='utf-8')}")
+    except binascii.Error as e:
+        print("| Base64: ERROR..")
+
+    print("\n=================  ROT  =================")
+    try:
+        print(f"| ROT13: {codecs.decode(words, 'rot13')}")
+    except TypeError as e:
+        print("| ROT13: ERROR...")
+    try:
+        print(f"| Base64(UTF-8) -> ROT13: {codecs.decode(base64.b64decode(words))}")
+    except:
+        print("| Base64(UTF-8) -> ROT13: ERROR...")
+
+    print("\n=================  Base58  =================")
+    try:
+        print(f"| Base58(Byte): {base64.b85decode(words)}")
+        print(f"| Base58(UTF-8): {base64.b58decode(words).decode(encoding='utf-8')}")
+    except:
+        print("| Base58: ERROR..")
